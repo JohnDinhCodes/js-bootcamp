b56490b61cd18c018f4802eaed14cdef752c61b7
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-16 17:41:28 -0400

    Completed temp-conversion.js

---
+++ basics/temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 50 //0c - 273.15k
+console.log(fahrenheit + '°F')
+
+//calculate celcius and store in celsius variable
+let celcius = (fahrenheit - 32) * (5 / 9)
+//print that value
+console.log(celcius + '°C')
+
+//calculate kelvin value and store in variable
+let kelvin = celcius + 273.15
+//print that value
+console.log(kelvin + 'K')
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+// 1. You can't define a variable more than once
+let petName = 'Tibbers'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. THere are rules related to variable names
+let test_ = 2
+let result = 3 + 4
+
+// 3. Variable names cannot be reserved keywords
+//let let = 12 is invalid
