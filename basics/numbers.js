049ce145d7669d608c40c5e6ff97ada78e63f174
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-16 14:07:28 -0400

    Completed Basics - Numbers

---
+++ basics/numbers.js
@@ -0,0 +1,12 @@
+let age = 20
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// Challenge Area
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
