fe7f477726039da0d6a0c4a7ff7b274569f8e424
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-17 00:08:14 -0400

    Completed Boolean

---
+++ basics/boolean.js
@@ -0,0 +1,26 @@
+// === - equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+let temp = 32
+let isFreezing = temp <= 32
+
+console.log(isFreezing)
+
+// Challenge area
+
+// Create age set to your age
+let age = 20
+// Calculate is child - if they are 7 or under
+let isChild = age <= 7
+// Calculate is senior - if they are 65 or older
+let isSenior = age >= 65
+// Print is child value
+console.log('You are a child - ' + isChild)
+// Print is senior value
+console.log('You are a senior - ' + isSenior)
+
+console.log('You are ' + age + ' years old.')
