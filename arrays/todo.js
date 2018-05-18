a527279b8f79ecea6d531366f68cc5c227de79bf
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-18 13:36:53 -0400

    Complete Arrays basics

---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items --> Todo: ...
+
+let todos = ['Study JavaScript', 'Study React', 'Update Portfolio', 'Learn PHP', 'Create CMS Website']
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
