90d50a88bb28d547cfce1a6c711182716f7fc91d
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-18 02:01:54 -0400

    Completed Object References

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,11 +1,14 @@
 let myAccount = {
     name: 'John Dinh',
     expenses: 0,
-    income: 0,
+    income: 0
 }
 
-let addExpense = function (account, ammount) {
-    account.expenses = account.expesnes + ammount
+let otherAccount = myAccount
+otherAccount.income = 1000
+
+let addExpense = function (account, amount) {
+    // account.expenses += amount
     console.log(account)
 }
 
