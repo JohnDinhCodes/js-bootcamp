12787ee523c112643980e1bd8254ce5c2e87b2a6
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-18 14:28:31 -0400

    Looping Over Arrays - with forEach

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,16 +1,11 @@
-// Create an array with five todos
-// You have x todos
-// Print the first and second to last items --> Todo: ...
-
 const todos = ['Study JavaScript', 'Study React', 'Update Portfolio', 'Learn PHP', 'Create CMS Website']
 
-// delete the 3rd item
-// Add a new item onto the end
-// Remove the first item from the list
-
-todos.splice(2, 1))
-todos.push('I am at the end'))
+todos.splice(2, 1)
+todos.push('I am at the end')
 todos.shift()
 
 console.log(`You have ${todos.length} todos`)
-console.log(todos)
+
+todos.forEach(function (todo, index) {
+    console.log(`${index + 1}: ${todo}`)
+})
