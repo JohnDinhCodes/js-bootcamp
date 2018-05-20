c818778400c4b8e29bae2381a740ba46fc920d04
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-20 13:17:25 -0400

    Complete Part one of Searching Arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modifications',
+    body: 'Get a new seat'
+}]
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -7,12 +16,22 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (note, index) {
-    console.log(index)
-    console.log(note)
-})
+// notes.forEach(function (note, index) {
+//     console.log(index)
+//     console.log(note)
+// })
 
 console.log(notes.length)
 console.log(notes)
+
+let someObject = {}
+let otherObject = someObject
+
+const index = notes.findIndex(function (note, index) {
+    console.log(note)
+    return note.title === 'Habbits to work on'
+})
+
+console.log(index)
