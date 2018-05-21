c79a15fb5a570e1875599f1ba18733890533abfc
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-21 03:09:27 -0400

    Completed "Adding Elements via the Dom"

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modifications',
+    body: 'Get a new seat'
+}]
+
 // DOM - Document Object Model
 
 // const paragraph = document.querySelector('p')
@@ -9,3 +20,8 @@ const paragraphs = document.querySelectorAll('p')
 paragraphs.forEach(function (p) {
     p.textContent = '********'
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/script.js
+++ todo-app/script.js
@@ -1,8 +1,34 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [{
+    title: 'Study JavaScript',
+    completed: true
+}, {
+    title: 'Study React',
+    completed: false
+}, {
+    title: 'Update Portfolio',
+    completed: true
+}, {
+    title: 'Learn PHP',
+    completed: false
+}, {
+    title: 'Create CMS Website',
+    completed: false
+}]
 
-
-paragraphs.forEach(function (p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
-    }
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 })
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.title
+    document.querySelector('body').appendChild(p)
+})
+
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
