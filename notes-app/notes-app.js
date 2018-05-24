f063a02fe6dcfa7230a3871a4ad429e91237ff2b
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-24 05:40:06 -0400

    Complete JavaScript Dates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -34,5 +34,19 @@ window.addEventListener('storage', function (e) {
     }
 })
 
-// 1. Setup link href to include hash with id
-// 2. Setup the assignn call to include has with id
+// Unix Epoch - January 1st 1070 00:00:00
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth() + 1}`)
+// console.log(`Day of month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+// 1. Create two dates in the past (use string for Date)
+// 2. Get timestamps for both
+// 3. Figure out which is first and print its time (use toString)
