bb3d53d850aa563c8a33c21da4e5b593ecdbc2b5
John Dinh <JohnDuyDinh@gmail.com>
2018-05-31 16:26:33 -0400

    Complete All 3 Projects

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
     if (response.status === 200) {
         const data = await response.json()
@@ -15,7 +15,7 @@ const getCurrentCountry = async () => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch(`https://restcountries.eu/rest/v2/all`)
+    const response = await fetch(`//restcountries.eu/rest/v2/all`)
 
     if (response.status === 200) {
         let data = await response.json()
@@ -32,7 +32,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=7dee97d6371ef9')
+    const response = await fetch('//ipinfo.io/json?token=7dee97d6371ef9')
 
     if (response.status === 200) {
         return response.json()
