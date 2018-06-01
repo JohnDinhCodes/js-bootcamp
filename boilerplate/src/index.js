bd547737723fc2f5e0162133cc0cf45f9d4c96d8
John Dinh <JohnDuyDinh@gmail.com>
2018-05-31 23:54:29 -0400

    Clean Webpack Boilerplate

--- boilerplate/src/index.js
+++
@@ -1,9 +0,0 @@
-// import './utilities.js'
-
-import square, { add, name } from './utilities'
-import scream from './scream'
-
-console.log('index.js')
-console.log(add(32, 3), name )
-console.log(scream(name))
-console.log(square(10))
