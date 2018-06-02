f345275d7dc4f682b3881b98a7519faa93f971b1
John Dinh <JohnDuyDinh@Gmail.com>
2018-06-01 23:18:08 -0400

    Re-add todo-app

---
+++ todo-app/scripts/todo-app.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let todos = getSavedTodos()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#create-todo').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const newTodo = (event.target.newTodo.value).trim()
+
+    if (newTodo.length > 0) {
+        todos.push({
+            id: uuidv4(),
+            title: newTodo,
+            completed: false
+        })
+        saveTodos()
+        renderTodos(todos, filters)
+        e.target.newTodo.value = ''   
+    }
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
+
